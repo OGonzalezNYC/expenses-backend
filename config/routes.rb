a2eaebeff053cf9f47516eb0ca6a1c6918398c24
github username <github email address>
2020-06-22 01:09:44 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :transactions
+      resources :accounts
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
+
+# Because of the above two levels of namespacing, no fetch request can simply be:
+# fetch('http://localhost:3000/transactions'); rather, it would need to be:
+# fetch('http://localhost:3000/api/v1/transactions')
